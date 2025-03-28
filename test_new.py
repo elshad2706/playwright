a8d68b5f1621b0c38a79bbbc1566dfac36f8a723
unknown <elshadmursalov8@gmail.com>
2025-03-29 00:04:23 +0300

    first my test playwright

---
+++ test_new.py
@@ -0,0 +1,30 @@
+# def test_inventory(page):
+#     response = page.request.get('https://petstore.swagger.io/v2/store/inventory')
+#     print("status ==", response.status)
+#     print("Response", response.json())
+#     print("text", response.text())
+#     print("body", response.body())
+
+
+def test_add_user(page):
+    data = [
+        {
+            "id": 9743,
+            "username": "fsd",
+            "firstName": "fff",
+            "lastName": "ggg",
+            "email": "bbb",
+            "password": "tt",
+            "phone": "333",
+            "userStatus": 0
+        }
+    ]
+    header = {
+        'accept': 'application/json',
+        'content-Type': 'application/json'
+    }
+    response = page.request.post('https://petstore.swagger.io/v2/user/createWithArray', data=data, headers=header)
+    print(response.status)
+    print(response.text())
+    print(response.json())
+    print(response.body())
