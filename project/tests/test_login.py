d6d376345e32e345ce0c495331f1989ec9eed444
unknown <elshadmursalov8@gmail.com>
2025-04-20 23:50:12 +0300

    add parametrize test

--- project/tests/test_login.py
+++ project/tests/test_login.py
@@ -1,10 +1,20 @@
-from project.pages.login_page import LoginPage
+# from project.pages.login_page import LoginPage
+# from project.pages.dashboard_page import DashboardPage
+# from playwright.sync_api import Page
+import pytest
 
 
-def test_login_failure(page):
-    login_page = LoginPage(page)
+def test_login_failure(login_page):
     login_page.navigate()
-    login_page.login("incorrect_login","incorrect_password")
+    login_page.login('user', 'password')
     assert login_page.get_error_message() == 'Invalid credentials. Please try again.'
 
 
+@pytest.mark.parametrize('username, password', [
+    ('user', 'user'),
+    ('admin', 'admin')
+])
+def test_login_success(login_page, dashboard_page, username, password):
+    login_page.navigate()
+    login_page.login(username, password)
+    dashboard_page.assert_welcome_message(f"Welcome {username}")
